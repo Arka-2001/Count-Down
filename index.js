8a79a328790e783e583cbef3f8b8c26eb93b4e66
Arka Das <98737740+Arka-2001@users.noreply.github.com>
2024-02-24 11:46:43 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+
+
+const enddate="24 July 2024 10:00:00 PM"
+
+document.getElementById("end-date").innerText = enddate;
+
+const inputs = document.querySelectorAll("input")
+    
+
+function clock() {
+    const end = new Date(enddate)
+    const now = new Date()
+    const diff = (end - now) / 1000;
+
+    if (diff < 0) return;
+
+    // convert into days;
+    inputs[0].value = Math.floor(diff / 3600 / 24);
+    inputs[1].value = Math.floor(diff / 3600) % 24;
+    inputs[2].value = Math.floor(diff / 60) % 60;
+    inputs[3].value = Math.floor(diff) % 60;
+}
+
+
+clock()
+
+/**
+ *  1 day = 24 hrs
+ *  1 hr = 60 mins
+ *  60 min = 3600 sec
+ */
+
+setInterval(
+    () => {
+        clock()
+    },
+    1000
+)
+
+
+
+
+
